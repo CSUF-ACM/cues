880d5330ac4e5122afcfc5333c462be7c59ab43d
Madison Jordan <madijrdn@gmail.com>
2019-04-22 15:32:06 -0700

    logic for streak and willpower calculations

---
+++ src/streak-calc.js
@@ -0,0 +1,34 @@
+//[HIGH] calc average streak ---> just display
+//
+// check if streak end,
+//store streak length in array (or container like dll)
+// sum array and current streak length
+// divide by size of array
+
+//[HIGH] calc highest streak  --> just display
+// check if highest (integrate into previous function).
+// maybe push to front of list if highest.
+
+//calc percentage success of each habit --> pie chart, bar chart (for all habits)
+//
+//  (container for answer per habit) -- > keep track each in array size 30 (days) per habit
+//
+//  Per habit (for past 7 days, 30 days)
+//  access --> num_yes / total_ans_collected
+//
+//  if bad habit, subtract from 1.0 (100%) to get complement (true success rate)
+//
+//  store in array size >= 4. (willpower per week in each index. 4 weeks = 28 days)
+//  [result to be used in willpower calc too]
+
+// calc willpower per week using weight  --> historical bar chart
+//
+//  get percent success for each habit from func
+//  sum if month calc
+//  multiply by weight of this habit (displayed as 1-10 to user. calculated as 0.1-1.0)
+//
+// sum all of the habit success totals
+//  divide by number of habits (habit container size)
+//convert to int if not already.
+
+//[LOW] determine next streak goal per habit. y=sqrt(x). ceiling function.
